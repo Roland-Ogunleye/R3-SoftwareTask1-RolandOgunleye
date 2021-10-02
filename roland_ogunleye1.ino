12978ae8ce074161b649e3bc8e848a60b3127f3d
Roland-Ogunleye <91505947+Roland-Ogunleye@users.noreply.github.com>
2021-10-02 16:32:26 -0400

    Update roland_ogunleye1.ino

--- roland_ogunleye1.ino
+++ roland_ogunleye1.ino
@@ -11,23 +11,32 @@ void setup()
 {
   pinMode(A0, INPUT);
   Serial.begin(9600);
-
+// Set the pins as outputs
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(4, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(10, OUTPUT);
 }
 
 void loop()
 {
-  
+  // Read in value from A0 and then map from a range of 0-99
   SensorValue = analogRead(A0);
   SensorValue = map(SensorValue, 0, 1023, -9,99);
+  
+  // Detect the first two digits of the Sensorvalue
   FirstDigit = (SensorValue / 10) % 10;
   SecondDigit = SensorValue %10;
+  //Display First, Second and Sensorvalue 
   Serial.println(SensorValue);
- //Serial.println(SecondDigit);
-  //Serial.println(FirstDigit);
+ Serial.println(SecondDigit);
+  Serial.println(FirstDigit);
+  
+  // If statements to check what first digit to display on seven segments
   if ( FirstDigit == 0) 
   {
         digitalWrite(5, LOW);
@@ -90,7 +99,7 @@ void loop()
     digitalWrite(7, LOW);
     digitalWrite(4, HIGH);
   }
-  
+  // If statements to check what second digit to display on seven segments
    if ( SecondDigit == 0) {
     digitalWrite(11, LOW);
     digitalWrite(12, LOW);
